99ac65bcb8d737ba0ffd009ee87afbb05215860f
Nick Sophinos <nick@wdeanmedical.com>
2024-10-08 18:29:59 -0400

    tab navigator and blank destination screens installed

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import MusicScreen from "./screens/MusicScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import SearchScreen from "./screens/SearchScreen";
+
+const Tab = createBottomTabNavigator()
+const Stack = createNativeStackNavigator()
 
 export default function App() {
+
+  function Tabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Music" component={MusicScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+      </Tab.Navigator>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>BasedMusic</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Tabs" children={() => <Tabs />} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 48
-  }
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Style from '../style'
+
+function HomeScreen() {
+  return (
+    <View style={Style.container}>
+      <Text style={Style.text}>Home Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
