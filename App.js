b34f9f9df000fba1ef02543b250a1dfdd2621006
ez-coder13 <amritvirrandhawa@gmail.com>
2024-10-09 23:15:10 -0700

    Reimplement Welcome Screen changes based on the latest main branch

--- App.js
+++ App.js
@@ -29,6 +29,7 @@ import ListenerProfileScreen from './screens/ListenerProfileScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import InboxScreen from './screens/InboxScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
@@ -52,30 +53,31 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer ref={navigationRef}>
-          <Stack.Navigator initialRouteName="SignIn">
-           <Stack.Screen name="SignUp" component={SignUpScreen} />
-           <Stack.Screen name="ConfirmSignUp" component={ConfirmSignUpScreen} />
-           <Stack.Screen name="SignIn" component={SignInScreen} />
-           <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-           <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
-           <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="ConfirmSignUp" component={ConfirmSignUpScreen} />
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+          <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
 
-           {/* Listener Profile Screens */}
-           <Stack.Screen name='ListenerProfile' component={ListenerProfileScreen} options={{ headerTitle: 'Listener Profile' }} />
-           <Stack.Screen name='Favorites' component={FavoritesScreen} options={{ headerTitle: 'Favorites' }} />
-           <Stack.Screen name='Settings' component={SettingsScreen} options={{ headerTitle: 'Settings' }} />
-           <Stack.Screen name='Inbox' component={InboxScreen} options={{ headerTitle: 'Inbox' }} />
+          {/* Listener Profile Screens */}
+          <Stack.Screen name='ListenerProfile' component={ListenerProfileScreen} options={{ headerTitle: 'Listener Profile' }} />
+          <Stack.Screen name='Favorites' component={FavoritesScreen} options={{ headerTitle: 'Favorites' }} />
+          <Stack.Screen name='Settings' component={SettingsScreen} options={{ headerTitle: 'Settings' }} />
+          <Stack.Screen name='Inbox' component={InboxScreen} options={{ headerTitle: 'Inbox' }} />
 
-           {/* Venue Profile screens */}
-           <Stack.Screen name='VenueProfileScreen' component={VenueProfileScreen} />
-           <Stack.Screen name="VenueInfo" component={VenueInfoScreen} />
-           <Stack.Screen name="Events" component={VenueEventsScreen} />
-           <Stack.Screen name="VenueBookings" component={VenueBookingsScreen} />
-           {/* Add other screens as needed */}
+          {/* Venue Profile screens */}
+          <Stack.Screen name='VenueProfileScreen' component={VenueProfileScreen} />
+          <Stack.Screen name="VenueInfo" component={VenueInfoScreen} />
+          <Stack.Screen name="Events" component={VenueEventsScreen} />
+          <Stack.Screen name="VenueBookings" component={VenueBookingsScreen} />
+          {/* Add other screens as needed */}
 
-           <Stack.Screen name="Tabs" children={() => <Tabs />} options={{ headerShown: false }} />
-          </Stack.Navigator>
-        </NavigationContainer>
+          <Stack.Screen name="Tabs" children={() => <Tabs />} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
