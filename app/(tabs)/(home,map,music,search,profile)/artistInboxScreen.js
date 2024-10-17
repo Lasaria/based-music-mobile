3bb17d7267e06db85689fcb6464179982a4794cd
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-10-17 00:09:42 -0600

    implemented all the UX design for artist profile and inbox screen

---
+++ app/(tabs)/(home,map,music,search,profile)/artistInboxScreen.js
@@ -0,0 +1,242 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { router } from "expo-router";
+import ArtistProfileScreen from "./artistProfile";
+
+const messagesData = [
+  {
+    id: 1,
+    name: "Marina Martinez",
+    message: "I'm watching Friends, what are u doin? 😍",
+    time: "34 min",
+   profileImage: require("../../../assets/images/profile3.jpg"),
+    unreadCount: 3,
+  },
+  {
+    id: 2,
+    name: "Lusy Ivka",
+    message: "I'm watching Friends, what are u doin? 👍",
+    time: "34 min",
+   profileImage: require("../../../assets/images/profile4.jpg"),
+    unreadCount: 1,
+  },
+  {
+    id: 3,
+    name: "Yana Kot",
+    message: "I'm watching Friends, what are u doin?",
+    time: "34 min",
+   profileImage: require("../../../assets/images/profile5.jpg"),
+    unreadCount: 2,
+  },
+];
+
+const recentContacts = [
+  {
+    id: 1,
+    name: "John Doe",
+   profileImage: require("../../../assets/images/profile6.jpg"),
+  },
+  {
+    id: 2,
+    name: "Stef",
+    profileImage: require("../../../assets/images/profile7.jpg"),
+  },
+  {
+    id: 3,
+    name: "Loran",
+    profileImage: require("../../../assets/images/profile8.jpg"),
+  },
+  {
+    id: 4,
+    name: "Simon",
+   profileImage: require("../../../assets/images/profile3.jpg"),
+  },
+];
+
+const ArtistInboxScreen = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const navigation = useNavigation();
+
+  const renderRecentContact = ({ item }) => (
+    <View style={styles.contactContainer}>
+      <Image source={item.profileImage} style={styles.contactImage} />
+      <Text style={styles.contactName}>{item.name}</Text>
+    </View>
+  );
+
+  const renderMessageItem = ({ item }) => (
+    <View style={styles.messageItem}>
+      <Image source={item.profileImage} style={styles.messageImage} />
+      <View style={styles.messageInfo}>
+        <Text style={styles.messageName}>{item.name}</Text>
+        <Text style={styles.messageText}>{item.message}</Text>
+      </View>
+      <View style={styles.messageMeta}>
+        <Text style={styles.messageTime}>{item.time}</Text>
+        {item.unreadCount > 0 && (
+          <View style={styles.unreadCountContainer}>
+            <Text style={styles.unreadCount}>{item.unreadCount}</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.push("/artistProfile")}>
+          <Text style={styles.backButton}>←</Text>
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>Messages</Text>
+        <TouchableOpacity>
+          <Text style={styles.editButton}>✎</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TextInput
+        style={styles.searchInput}
+        placeholder="search"
+        placeholderTextColor="#888"
+        value={searchText}
+        onChangeText={setSearchText}
+      />
+
+      <FlatList
+        data={recentContacts}
+        renderItem={renderRecentContact}
+        keyExtractor={(item) => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.recentContactsContainer}
+      />
+      <Text style={styles.recentText}>Recent</Text>
+      <FlatList
+        data={messagesData}
+        renderItem={renderMessageItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.messagesContainer}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+  backButton: {
+    color: "#fff",
+    fontSize: 30,
+  },
+  headerTitle: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  editButton: {
+    color: "#fff",
+    fontSize: 24,
+  },
+  recentContactsContainer: {
+    paddingBottom: 5, 
+    marginBottom: -20, 
+  },
+  searchInput: {
+    backgroundColor: "#1e1e1e",
+    borderRadius: 25,
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    color: "#fff",
+    marginBottom: 10,
+  },
+  contactContainer: {
+    alignItems: "center",
+    marginRight: 15,
+  },
+  contactImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  contactName: {
+    color: "#fff",
+    marginTop: 5,
+    fontSize: 12,
+  },
+  recentText: {
+    color: 'white',
+    fontSize: 20,
+    marginTop: -260,
+    marginBottom: 10, 
+  },
+  messagesContainer: {
+    paddingBottom: 10,
+  },
+  messageItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#333",
+  },
+  messageImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  messageInfo: {
+    flex: 1,
+    marginLeft: 15,
+  },
+  messageName: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  messageText: {
+    color: "#888",
+  },
+  messageMeta: {
+    alignItems: "flex-end",
+  },
+  messageTime: {
+    color: "#888",
+    fontSize: 12,
+  },
+  unreadCountContainer: {
+    backgroundColor: "#6e00ff",
+    borderRadius: 15,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    marginTop: 5,
+  },
+  unreadCount: {
+    color: "#fff",
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+});
+
+export default ArtistInboxScreen;
---
+++ app/(tabs)/(home,map,music,search,profile)/artistProfile.js
@@ -0,0 +1,511 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Icon } from "react-native-elements";
+import { router } from "expo-router";
+
+const ArtistProfileScreen = () => {
+  const [followers, setFollowers] = useState("321k");
+  const [streams, setStreams] = useState("2.4M");
+  const [track, setTracks] = useState("125");
+  const [area1, setArea1] = useState("#25");
+  const [area2, setArea2] = useState("#8");
+  const [area3, setArea3] = useState("#2");
+
+  const navigation = useNavigation();
+  const [profileImage, setProfileImage] = useState(
+    require("../../../assets/images/profile.png")
+  );
+
+  const [isMusicSelected, setIsMusicSelected] = useState(true);
+
+  // Sample data for tracks and albums
+  const tracks = [
+    {
+      id: "1",
+      title: "Best friend",
+      artist: "Luna bay",
+      duration: "3:24",
+      imageUri: require("../../../assets/images/profile7.jpg"),
+    },
+    {
+      id: "2",
+      title: "Odd one out",
+      artist: "Bod Moses",
+      duration: "3:24",
+      imageUri: require("../../../assets/images/profile3.jpg"),
+    },
+    {
+      id: "3",
+      title: "Need you now",
+      artist: "Joji",
+      duration: "3:24",
+      imageUri: require("../../../assets/images/profile4.jpg"),
+    },
+  ];
+
+  const albums = [
+    {
+      id: "1",
+      title: "Desert rose",
+      artist: "Lady A",
+      imageUri: require("../../../assets/images/profile.png"),
+    },
+    {
+      id: "2",
+      title: "Infinity",
+      artist: "Labirinth",
+      imageUri: require("../../../assets/images/profile2.jpg"),
+    },
+    {
+      id: "3",
+      title: "Not Like US",
+      artist: "kendrick",
+      imageUri: require("../../../assets/images/profile5.jpg"),
+    },
+    {
+      id: "4",
+      title: "Beliver",
+      artist: "Sabrina",
+      imageUri: require("../../../assets/images/profile3.jpg"),
+    },
+  ];
+
+  const beats = [
+    {
+      id: "1",
+      title: "Instrument 3",
+      artist: "Artist",
+      price: "$32.00",
+      popular: true,
+      imageUri: require("../../../assets/images/profile4.jpg"),
+    },
+    {
+      id: "2",
+      title: "Instrument 1",
+      artist: "Artist",
+      price: "$0.00",
+      free: true,
+      imageUri: require("../../../assets/images/profile3.jpg"),
+    },
+    {
+      id: "3",
+      title: "Instrument 2",
+      artist: "Artist",
+      price: "$32.00",
+      imageUri: require("../../../assets/images/profile2.jpg"),
+    },
+  ];
+
+  const handleMusicToggle = () => {
+    setIsMusicSelected(true);
+  };
+
+  const handleAboutToggle = () => {
+    setIsMusicSelected(false);
+  };
+
+  return (
+    <View style={styles.outerContainer}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.container}>
+          {/* Large background image */}
+          <View style={styles.largeImageContainer}>
+            <Image
+              source={require("../../../assets/images/profile1.png")}
+              style={styles.largeImage}
+            />
+
+            {/* Small circular image with edit button */}
+            <View style={styles.profileOverlay}>
+              <Image source={profileImage} style={styles.profileImage} />
+
+              {/* Artist Name and Info */}
+              <Text style={styles.artistName}>Jacob Lee</Text>
+              <Text style={styles.artistInfo}>Artist / Musician / Writer</Text>
+            </View>
+
+            {/* Stats section */}
+            <View style={{ flexDirection: "row" }}>
+              <View style={[styles.statsOverlay, { paddingBottom: 40 }]}>
+                <View style={styles.statItem}>
+                  <Text style={styles.statValue}>{followers}</Text>
+                  <Text style={styles.statLabel}>followers</Text>
+                </View>
+                <View style={styles.statItem}>
+                  <Text style={styles.statValue}>{streams}</Text>
+                  <Text style={styles.statLabel}>streams</Text>
+                </View>
+                <View style={styles.statItem}>
+                  <Text style={styles.statValue}>{track}</Text>
+                  <Text style={styles.statLabel}>tracks</Text>
+                </View>
+              </View>
+              <View style={styles.statsOverlay}>
+                <View style={[styles.statItem, { paddingLeft: 10 }]}>
+                  <Text style={styles.statValue}>{area1}</Text>
+                  <Text style={styles.statLabel}>DMV</Text>
+                </View>
+                <View style={[styles.statItem, { paddingLeft: 20 }]}>
+                  <Text style={styles.statValue}>{area2}</Text>
+                  <Text style={styles.statLabel}>Virginia</Text>
+                </View>
+                <View style={styles.statItem}>
+                  <Text style={styles.statValue}>{area3}</Text>
+                  <Text style={styles.statLabel}>Arlington</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          {/* Buttons */}
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.uploadButton}>
+              <View style={{ flexDirection: "row" }}>
+                <Icon name="upload" color="white" />
+                <Text style={styles.buttonText}>Upload</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.inboxButton}
+              onPress={() => router.push("/artistInboxScreen")}
+            >
+              <View style={{ flexDirection: "row" }}>
+                <Icon name="mail" color="white" />
+                <Text style={styles.buttonText}>Inbox</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          {/* Music/About Toggle */}
+          <View style={styles.toggleContainer}>
+            <TouchableOpacity
+              style={[
+                styles.toggleButton,
+                isMusicSelected && styles.activeButton,
+              ]}
+              onPress={handleMusicToggle}
+            >
+              <Text
+                style={[
+                  styles.toggleText,
+                  isMusicSelected && styles.activeText,
+                ]}
+              >
+                Music
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                styles.toggleButton,
+                !isMusicSelected && styles.activeButton,
+              ]}
+              onPress={handleAboutToggle}
+            >
+              <Text
+                style={[
+                  styles.toggleText,
+                  !isMusicSelected && styles.activeText,
+                ]}
+              >
+                About
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Content under the toggle */}
+          {isMusicSelected ? (
+            <>
+              {/* Tracks Section */}
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>TRACKS</Text>
+                {tracks.map((track) => (
+                  <View key={track.id} style={styles.trackItem}>
+                    <Image source={track.imageUri} style={styles.trackImage} />
+                    <View style={styles.trackInfo}>
+                      <Text style={styles.trackTitle}>{track.title}</Text>
+                      <Text style={styles.trackArtist}>{track.artist}</Text>
+                    </View>
+                    <Text style={styles.trackDuration}>{track.duration}</Text>
+                  </View>
+                ))}
+              </View>
+
+              {/* Albums Section */}
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>ALBUMS</Text>
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                  {albums.map((album) => (
+                    <View key={album.id} style={styles.albumItem}>
+                      <Image
+                        source={album.imageUri}
+                        style={styles.albumImage}
+                      />
+                      <Text style={styles.albumTitle}>{album.title}</Text>
+                      <Text style={styles.albumArtist}>{album.artist}</Text>
+                    </View>
+                  ))}
+                </ScrollView>
+              </View>
+
+              {/** Beats sections */}
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Beats</Text>
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                  {beats.map((beat) => (
+                    <View key={beat.id} style={styles.beatItem}>
+                      <Image
+                        source={beat.imageUri}
+                        style={styles.beatImage} 
+                      />
+                         <Text style={styles.beatArtist}>{beat.price}</Text>
+                      <Text style={styles.beatTitle}>{beat.title}</Text>
+                      <Text style={styles.beatArtist}>{beat.artist}</Text>
+                      <Text style={styles.beatPrice}>
+                        {beat.popular
+                          ? "Popular"
+                          : beat.free
+                          ? "Free"
+                          : beat.price}
+                      </Text>
+                    </View>
+                  ))}
+                </ScrollView>
+              </View>
+            </>
+          ) : (
+            <View>
+              {/* About Section */}
+              <Text style={styles.aboutText}>
+                This is the about section content.
+              </Text>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: 20,
+    backgroundColor: "#000",
+  },
+  largeImageContainer: {
+    position: "relative",
+    width: "100%",
+    height: 300,
+  },
+  container: {
+    backgroundColor: "#000",
+  },
+  largeImage: {
+    width: "100%",
+    height: 350,
+    resizeMode: "cover",
+    borderRadius: 10,
+  },
+  profileOverlay: {
+    position: "absolute",
+    top: "30%",
+    left: 0,
+    right: 0,
+    alignItems: "center",
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    borderWidth: 3,
+    borderColor: "#fff",
+  },
+  editButton: {
+    marginTop: 10,
+    backgroundColor: "#6e00ff",
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderRadius: 20,
+  },
+  editButtonText: {
+    color: "#fff",
+    fontSize: 14,
+  },
+  artistName: {
+    color: "#fff",
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 5,
+  },
+  artistInfo: {
+    color: "#bbb",
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  statsOverlay: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    bottom: 2,
+    position: "absolute",
+    left: 0,
+    right: 0,
+  },
+  statItem: {
+    alignItems: "center",
+  },
+  statValue: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  statLabel: {
+    color: "#bbb",
+    fontSize: 12,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 60,
+  },
+  uploadButton: {
+    backgroundColor: "#6e00ff",
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+  },
+  inboxButton: {
+    backgroundColor: "#666",
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    marginLeft: 5,
+  },
+  toggleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  toggleButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderBottomWidth: 2,
+    borderColor: "transparent",
+  },
+  activeButton: {
+    borderColor: "#6e00ff",
+  },
+  toggleText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  activeText: {
+    color: "#6e00ff",
+    fontWeight: "bold",
+  },
+  sectionContainer: {
+    marginVertical: 10,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  trackItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 15,
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: "grey",
+    width: "100%",
+    padding: 5,
+  },
+  trackImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+  trackInfo: {
+    marginLeft: -70,
+    justifyContent: "space-around",
+  },
+  trackTitle: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  trackArtist: {
+    color: "#bbb",
+    fontSize: 14,
+  },
+  trackDuration: {
+    color: "#bbb",
+    fontSize: 12,
+    marginLeft: 100,
+    textAlign: "right",
+    justifyContent: "flex-end",
+  },
+  albumItem: {
+    marginRight: 10,
+    alignItems: "center",
+  },
+  albumImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 30,
+    marginBottom: 5,
+  },
+  albumTitle: {
+    color: "#fff",
+    fontSize: 14,
+  },
+  albumArtist: {
+    color: "#bbb",
+    fontSize: 12,
+  },
+  aboutText: {
+    color: "#fff",
+    fontSize: 16,
+    padding: 20,
+  },
+  beatItem: {
+    marginRight: 10,
+    alignItems: "center",
+  },
+  beatImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 10,
+    marginBottom: 5,
+  },
+  beatTitle: {
+    color: "#fff",
+    fontSize: 14,
+  },
+  beatArtist: {
+    color: "#bbb",
+    fontSize: 12,
+  },
+});
+
+export default ArtistProfileScreen;
