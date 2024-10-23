3e4ca63734637bc51adf47c452814af5050eb18e
Wassim Lkorchy <lacorchy77@gmail.com>
2024-10-23 15:27:13 -0400

    Fixed Input component CSS

--- app/signIn.js
+++ app/signIn.js
@@ -1,5 +1,13 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { View, Text, StyleSheet, Alert, Dimensions, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  Dimensions,
+  SafeAreaView,
+  Image,
+} from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { useNavigation } from "@react-navigation/native";
@@ -7,14 +15,18 @@ import { AuthService } from "../services/AuthService";
 import { axiosPost } from "../utils/axiosCalls";
 import { Colors } from "../constants/Color";
 import InputComponent from "../components/InputComponent";
-import { Ionicons, Feather, AntDesign, SimpleLineIcons } from '@expo/vector-icons';
+import {
+  Ionicons,
+  Feather,
+  AntDesign,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
 import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
 import ButtonComponent from "../components/ButtonComponents";
-import { router } from 'expo-router';
-
+import { router } from "expo-router";
 
 WebBrowser.maybeCompleteAuthSession();
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 // Replace with your actual Google Client ID
 const GOOGLE_CLIENT_ID =
@@ -41,41 +53,41 @@ const SignInScreen = () => {
     scopes: ["profile", "email", "openid"],
   });
 
-
   useEffect(() => {
-    if (response?.type === 'success') {
+    if (response?.type === "success") {
       const { id_token } = response.params;
       handleGoogleSignIn(id_token);
     }
   }, [response]);
 
-
-  const handleGoogleSignIn = useCallback(async (idToken) => {
-    console.log(idToken)
-    try {
-      const result = await AuthService.googleSignIn(idToken);
-      console.log(result);
-      if (result.userCreated) {
-        router.push("userTypeChoice");
-      } else {
-        router.back()
-        router.replace("/homeIndex");
-        // router.setParams({ index: 0 })
+  const handleGoogleSignIn = useCallback(
+    async (idToken) => {
+      console.log(idToken);
+      try {
+        const result = await AuthService.googleSignIn(idToken);
+        console.log(result);
+        if (result.userCreated) {
+          router.push("userTypeChoice");
+        } else {
+          router.back();
+          router.replace("/homeIndex");
+          // router.setParams({ index: 0 })
+        }
+      } catch (error) {
+        console.error("Google Sign-In Error:", error);
+        Alert.alert(
+          "Sign In Error",
+          error.message || "An error occurred during Google sign-in."
+        );
       }
-    } catch (error) {
-      console.error('Google Sign-In Error:', error);
-      Alert.alert(
-        "Sign In Error",
-        error.message || "An error occurred during Google sign-in."
-      );
-    }
-  }, [navigation]);
-
+    },
+    [navigation]
+  );
 
   const validateInputs = () => {
-    setEmailError('');
-    setPasswordError('');
-    setErrorMessage('');
+    setEmailError("");
+    setPasswordError("");
+    setErrorMessage("");
 
     let isValid = true;
 
@@ -98,17 +110,17 @@ const SignInScreen = () => {
 
     if (!isValid) return; // If not valid, exit the function
 
-    setLoading(true) // Start loading
+    setLoading(true); // Start loading
 
     try {
       await AuthService.signIn(email, password);
-      router.back()
+      router.back();
       router.replace("/homeIndex");
       // Reset EMAIL and PASSWORD input fields
-      setEmail('');
-      setPassword('');
+      setEmail("");
+      setPassword("");
     } catch (err) {
-      setErrorMessage(err.message || 'An error occurred during sign-in.');
+      setErrorMessage(err.message || "An error occurred during sign-in.");
     } finally {
       setLoading(false); // Stop loading
     }
@@ -118,7 +130,7 @@ const SignInScreen = () => {
   const handleEmailChange = (text) => {
     setEmail(text);
     if (text) {
-      setEmailError(''); // Clear email error when user starts typing
+      setEmailError(""); // Clear email error when user starts typing
     }
   };
 
@@ -126,20 +138,24 @@ const SignInScreen = () => {
   const handlePasswordChange = (text) => {
     setPassword(text);
     if (text) {
-      setPasswordError(''); // Clear password error when user starts typing
+      setPasswordError(""); // Clear password error when user starts typing
     }
   };
-
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.signInView}>
         <Text style={styles.welcomeBackTitle}>Welcome Back</Text>
         <View style={styles.musicLogoContainer}>
-          <Image source={require('../assets/images/music.png')} style={styles.musicLogo} />
+          <Image
+            source={require("../assets/images/music.png")}
+            style={styles.musicLogo}
+          />
         </View>
 
-        {errorMessage ? <Text style={styles.errorMessageText}>{errorMessage}</Text> : null}
+        {errorMessage ? (
+          <Text style={styles.errorMessageText}>{errorMessage}</Text>
+        ) : null}
         {emailError ? <Text style={styles.errorText}>{emailError}</Text> : null}
         <InputComponent
           placeholder="Email"
@@ -149,36 +165,34 @@ const SignInScreen = () => {
           keyboardType="email-address"
           autoCapitalize="none"
           secureTextEntry={false}
+          style={styles.emailInput}
           emailError={emailError}
         />
 
-        {passwordError ? <Text style={styles.errorText}>{passwordError}</Text> : null}
+        {passwordError ? (
+          <Text style={styles.errorText}>{passwordError}</Text>
+        ) : null}
         <View style={styles.inputWithIconContainer}>
           <InputComponent
             placeholder="Password"
             placeholderTextColor={Colors.secondary}
             value={password}
-            onChangeText={handlePasswordChange} // Updated to handle password change
+            onChangeText={handlePasswordChange} // Updated to handle password
+            change
             keyboardType="default"
             autoCapitalize="none"
             secureTextEntry={!showPassword}
+            isPassword={true}
             passwordError={passwordError}
             style={styles.passwordInput}
-          />
-          <Ionicons
-            name={showPassword ? "eye-off-outline" : "eye-outline"}
-            size={24}
-            color={Colors.secondary}
-            onPress={() => setShowPassword(!showPassword)}
-            style={styles.togglePassIcon}
-          />
+          ></InputComponent>
         </View>
 
         <View style={styles.signInControls}>
           <View style={styles.radioButtonControl}>
             <RadioButtonGroup
               containerStyle={{ marginBottom: 10 }}
-              selected={''}
+              selected={""}
               onSelected={() => setCurrent(!current)}
               radioBackground={Colors.primary}
             >
@@ -195,10 +209,20 @@ const SignInScreen = () => {
               )}
             </RadioButtonGroup>
           </View>
-          <Text onPress={() => router.push("forgotPassword")} style={styles.forgotPasswordText}>Forgot password?</Text>
+          <Text
+            onPress={() => router.push("forgotPassword")}
+            style={styles.forgotPasswordText}
+          >
+            Forgot password?
+          </Text>
         </View>
 
-        <ButtonComponent onPress={handleEmailPasswordSignIn} title={'Sign In'} buttonStyle={styles.signInButton} textStyle={styles.signInButtonText} />
+        <ButtonComponent
+          onPress={handleEmailPasswordSignIn}
+          title={"Sign In"}
+          buttonStyle={styles.signInButton}
+          textStyle={styles.signInButtonText}
+        />
 
         <View style={styles.socialLayout}>
           <Text style={styles.signInWithSocialsText}>or sign in with</Text>
@@ -206,26 +230,43 @@ const SignInScreen = () => {
             <ButtonComponent
               onPress={{}}
               title={<Feather name="facebook" size={24} color={Colors.white} />}
-              buttonStyle={styles.socialButton} />
+              buttonStyle={styles.socialButton}
+            />
             <ButtonComponent
               onPress={() => promptAsync()}
-              title={<SimpleLineIcons name="social-google" size={24} color={Colors.white} />}
-              buttonStyle={styles.socialButton} />
+              title={
+                <SimpleLineIcons
+                  name="social-google"
+                  size={24}
+                  color={Colors.white}
+                />
+              }
+              buttonStyle={styles.socialButton}
+            />
             <ButtonComponent
               onPress={{}}
-              title={<AntDesign name="apple-o" size={24} color={Colors.white} />}
+              title={
+                <AntDesign name="apple-o" size={24} color={Colors.white} />
+              }
               buttonStyle={styles.socialButton}
             />
           </View>
         </View>
 
-        <Text style={styles.noAccountText}>Don't have an account?
-          <Text style={styles.boldSignUpText} onPress={() => router.push("signUp")}> Sign Up</Text>
+        <Text style={styles.noAccountText}>
+          Don't have an account?
+          <Text
+            style={styles.boldSignUpText}
+            onPress={() => router.push("signUp")}
+          >
+            {" "}
+            Sign Up
+          </Text>
         </Text>
       </View>
     </SafeAreaView>
   );
-}
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -234,49 +275,59 @@ const styles = StyleSheet.create({
   errorText: {
     fontSize: width * 0.04,
     color: Colors.error,
-    marginLeft: width * 0.11,
+    // marginLeft: width * 0.11,
     marginBottom: -width * 0.02,
   },
   errorMessageText: {
     fontSize: width * 0.04,
     color: Colors.error,
-    textAlign: 'center',
+    textAlign: "center",
+  },
+  inputWithIconContainer: {
+    flexDirection: "row", // Align the input and icon horizontally
+    alignItems: "center", // Vertically center align the input and icon
+    justifyContent: "space-between", // Ensure the icon is placed at the end
+    borderBottomWidth: 1,
+    borderColor: Colors.secondary,
   },
   signInView: {
     flex: 1,
-    paddingHorizontal: width * 0.05,
-    justifyContent: 'center',
+    paddingHorizontal: width * 0.08,
+    justifyContent: "center",
     gap: height * 0.02,
   },
   inputWithIconContainer: {
-    position: 'relative',
+    flexDirection: "row", // Align the input and icon horizontally
+    alignItems: "center", // Vertically center align the input and icon
+    justifyContent: "space-between", // Ensure the icon is placed at the end
+    borderBottomWidth: 1,
+    paddingVertical: 10,
   },
   passwordInput: {
-    paddingRight: 50, // Add space for the toggle icon
+    flex: 1, // Take the available space for the input
+    paddingVertical: 10,
+    color: Colors.white,
   },
   togglePassIcon: {
-    position: 'absolute',
-    right: 40,
-    top: '50%',
-    transform: [{ translateY: -12 }], // Align icon vertically
+    paddingHorizontal: 10,
   },
   musicLogoContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   welcomeBackTitle: {
     fontSize: width * 0.08,
     color: Colors.white,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   musicLogo: {
     height: height * 0.2,
     width: height * 0.2,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   signInButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: Colors.secondary,
     marginHorizontal: width * 0.05,
     height: height * 0.08,
@@ -284,30 +335,30 @@ const styles = StyleSheet.create({
   },
   signInButtonText: {
     fontSize: width * 0.04,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   signInControls: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginHorizontal: width * 0.05,
   },
   radioButtonControl: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: width * 0.02,
   },
   rememberMeText: {
     color: Colors.white,
-    fontSize: width * 0.030,
+    fontSize: width * 0.03,
   },
   forgotPasswordText: {
     color: Colors.white,
-    fontSize: width * 0.030,
+    fontSize: width * 0.03,
   },
   socialLayout: {
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    alignItems: "center",
+    justifyContent: "space-around",
     height: width * 0.35,
   },
   signInWithSocialsText: {
@@ -315,14 +366,14 @@ const styles = StyleSheet.create({
     fontSize: width * 0.04,
   },
   socialButtonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     gap: width * 0.05,
   },
   socialButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     height: height * 0.07,
     borderWidth: 1,
     borderColor: Colors.secondary,
@@ -330,29 +381,29 @@ const styles = StyleSheet.create({
     width: height * 0.07,
   },
   boldSignUpText: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: Colors.white,
   },
   noAccountText: {
-    textAlign: 'center',
+    textAlign: "center",
     color: Colors.secondary,
     marginTop: height * 0.03,
     fontSize: width * 0.04,
   },
   loadingContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   loadingImage: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
 });
 
--- components/InputComponent.js
+++ components/InputComponent.js
@@ -1,84 +1,133 @@
-import { StyleSheet, TextInput, View, Animated, Dimensions } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { Colors } from '../constants/Color';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Animated,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Colors } from "../constants/Color";
+import { Ionicons } from "@expo/vector-icons";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
-const InputComponent = ({ placeholder, value, onChangeText, keyboardType, autoCapitalize, secureTextEntry, emailError, passwordError }) => {
-    const [isFocused, setIsFocused] = useState(false);
-    const animatedLabel = new Animated.Value(value ? 1 : 0);
+const InputComponent = ({
+  placeholder,
+  value,
+  onChangeText,
+  keyboardType,
+  autoCapitalize,
+  secureTextEntry,
+  emailError,
+  passwordError,
+  isPassword,
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); // Password visibility toggle
+  const animatedLabel = new Animated.Value(value ? 1 : 0);
 
-    useEffect(() => {
-        Animated.timing(animatedLabel, {
-            toValue: isFocused || value ? 1 : 0,
-            duration: 200,
-            useNativeDriver: false,
-        }).start();
-    }, [isFocused, value]);
+  useEffect(() => {
+    Animated.timing(animatedLabel, {
+      toValue: isFocused || value ? 1 : 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  }, [isFocused, value]);
 
-    const labelStyle = {
-        position: 'absolute',
-        left: width * 0.05,
-        top: animatedLabel.interpolate({
-            inputRange: [0, 1],
-            outputRange: [height * 0.02, -height * 0.010],
-        }),
-        fontSize: animatedLabel.interpolate({
-            inputRange: [0, 1],
-            outputRange: [width * 0.045, width * 0.035],
-        }),
-        color: animatedLabel.interpolate({
-            inputRange: [0, 1],
-            outputRange: [Colors.secondary, Colors.white],
-        }),
-        backgroundColor: Colors.background,
-        paddingHorizontal: 4,
-    };
+  const labelStyle = {
+    position: "absolute",
+    left: width * 0.05,
+    top: animatedLabel.interpolate({
+      inputRange: [0, 1],
+      outputRange: [height * 0.02, -height * 0.01],
+    }),
+    fontSize: animatedLabel.interpolate({
+      inputRange: [0, 1],
+      outputRange: [width * 0.045, width * 0.035],
+    }),
+    color: animatedLabel.interpolate({
+      inputRange: [0, 1],
+      outputRange: [Colors.secondary, Colors.white],
+    }),
+    backgroundColor: Colors.background,
+    paddingHorizontal: 4,
+  };
 
-    return (
-        <View style={[styles.inputContainer, isFocused && styles.inputIsFocused, (emailError || passwordError) && styles.errorBorderColor]}>
-            <Animated.Text style={labelStyle}>{placeholder}</Animated.Text>
-            <TextInput
-                style={styles.input}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                value={value}
-                onChangeText={onChangeText}
-                keyboardType={keyboardType}
-                autoCapitalize={autoCapitalize}
-                secureTextEntry={secureTextEntry}
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword); // Toggle password visibility
+  };
+
+  return (
+    <View
+      style={[
+        styles.inputContainer,
+        isFocused && styles.inputIsFocused,
+        (emailError || passwordError) && styles.errorBorderColor,
+      ]}
+    >
+      <Animated.Text style={labelStyle}>{placeholder}</Animated.Text>
+      <View style={styles.inputWithIcon}>
+        <TextInput
+          style={styles.input}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          value={value}
+          onChangeText={onChangeText}
+          keyboardType={keyboardType}
+          autoCapitalize={autoCapitalize}
+          secureTextEntry={isPassword && !showPassword} // Toggle secure entry for password fields
+        />
+        {isPassword && (
+          <TouchableOpacity onPress={togglePasswordVisibility}>
+            <Ionicons
+              name={showPassword ? "eye-off-outline" : "eye-outline"}
+              size={24}
+              color={Colors.secondary}
+              style={styles.icon}
             />
-        </View>
-    );
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
 };
 
 export default InputComponent;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        marginVertical: height * 0.001,
-        borderWidth: 1, // Full border
-        borderColor: Colors.secondary,
-        borderRadius: 14,
-        position: 'relative',
-        paddingTop: height * 0.02,
-        paddingBottom: height * 0.02,
-        display: 'flex',
-        width: 343,
-        flexDirection: 'column',
-    },
-    input: {
-        height: height * 0.03,
-        paddingHorizontal: width * 0.05,
-        fontSize: width * 0.04,
-        color: Colors.white,
-    },
-    inputIsFocused: {
-        borderWidth: 2,
-        borderColor: Colors.white,
-    },
-    errorBorderColor: {
-        borderWidth: 1, // Full border
-        borderColor: Colors.error,
-    }
+  inputContainer: {
+    marginVertical: height * 0.001,
+    borderWidth: 1, // Full border
+    borderColor: Colors.secondary,
+    borderRadius: 14,
+    position: "relative",
+    paddingTop: height * 0.02,
+    paddingBottom: height * 0.02,
+    display: "flex",
+    width: 343,
+    flexDirection: "column",
+  },
+  input: {
+    height: height * 0.03,
+    paddingHorizontal: width * 0.05,
+    fontSize: width * 0.04,
+    color: Colors.white,
+    flex: 1, // Ensures input field takes up available space
+  },
+  inputWithIcon: {
+    flexDirection: "row",
+    alignItems: "center", // Aligns icon vertically with input
+  },
+  icon: {
+    paddingHorizontal: 10,
+  },
+  inputIsFocused: {
+    borderWidth: 2,
+    borderColor: Colors.white,
+  },
+  errorBorderColor: {
+    borderWidth: 1, // Full border
+    borderColor: Colors.error,
+  },
 });
