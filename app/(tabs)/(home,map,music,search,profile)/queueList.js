fde71790feaef28c06d34e5ff5189935e76bbf03
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-10-28 18:07:51 -0600

    dummy queue list screen

---
+++ app/(tabs)/(home,map,music,search,profile)/queueList.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { center } from '@turf/turf'
+
+const queueList = () => {
+  return (
+    <View style={{flex: 1, justifyContent:'center', alignItems:'center'}}>
+      <Text>queueList</Text>
+    </View>
+  )
+}
+
+export default queueList
