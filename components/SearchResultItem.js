b60922402e4cc92f92f31657df4d09dc5299d4f4
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-11-13 16:42:23 -0700

    all puhsed

--- components/SearchResultItem.js
+++ components/SearchResultItem.js
@@ -24,7 +24,7 @@ const SearchResultItem = ({ item, onPress,disablePlay = false }) => {
     const minutes = Math.floor(duration / 60);
     const seconds = Math.floor(duration % 60);
     return `${minutes}:${seconds.toString().padStart(2, "0")}`;
-  };
+  }
 
   const handlePress = () => {
     if (disablePlay) {
